56457d62ccf1c032910f88b36cefa08baa47a0e5
Tom Schoenfelder <mod4@gmx.de>
2022-04-12 18:04:27 +0200

    Fixed nested var in all.yml.example

--- prepare_host.sh
+++ prepare_host.sh
@@ -4,4 +4,4 @@ USAGE="Usage: prepare_host.sh HOSTNAME REMOTEUSER"
 
 [ $# -eq 2 ] || { echo $USAGE >&2; exit 1; }
 
-ansible-playbook -vvv -i $1, --extra-vars "ansible_user=$2" prepare.yml
+ansible-playbook -i $1, --extra-vars "ansible_user=$2" prepare.yml
